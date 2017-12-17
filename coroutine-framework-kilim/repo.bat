929a7f184fd9b8701725342b6ed51f0c743c87b1
Nepxion <1394997@qq.com>
2017-12-17 21:42:13 +0800

    上传kilim包

--- coroutine-framework-kilim/repo.bat
+++ coroutine-framework-kilim/repo.bat
@@ -1,1 +1,1 @@
-call mvn install:install-file -DgroupId=kilim -DartifactId=kilim -Dversion=1.0.0 -Dfile=repo/kilim-1.0.0.jar -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=kilim -DartifactId=kilim -Dversion=1.0.0 -Dfile=repo/kilim-1.0.0-jdk-1.7.0.jar -Dpackaging=jar -DgeneratePom=true
