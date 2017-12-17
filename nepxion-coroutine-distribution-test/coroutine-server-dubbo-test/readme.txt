d1d98f71864e6cbea3778ace6ab48de147449611
Nepxion <1394997@qq.com>
2017-12-17 21:20:25 +0800

    重构目录结构

--- nepxion-coroutine-distribution-test/coroutine-server-dubbo-test/readme.txt
+++
@@ -1,2 +0,0 @@
-﻿1.此测试用例只能跑在JDK 1.7或者以上版本
-2.执行mvn clean install编译出war，放到Tomcat或者JBoss下运行即可
--- nepxion-coroutine-performance-test/coroutine-client/install.bat
+++
@@ -1,7 +0,0 @@
-copy ..\coroutine-server\target\nepxion-coroutine-server-performance-test-1.0.0.jar lib\
-
-call mvn clean install
-
-copy target\nepxion-coroutine-client-performance-test-1.0.0.jar lib\
-
-pause
