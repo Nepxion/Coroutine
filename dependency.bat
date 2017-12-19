2c51dc79dee6c865b511300f39c31c7cdb2a3b4c
Nepxion <1394997@qq.com>
2017-12-19 21:41:00 +0800

    修改Bat

--- dependency.bat
+++ dependency.bat
@@ -17,25 +17,25 @@
 
 call mvn dependency:tree
 
-REM 查看包结构树
-REM call mvn dependency:tree -Dverbose -Dincludes=org.apache.curator:curator-recipes
+rem 查看包结构树
+rem call mvn dependency:tree -Dverbose -Dincludes=org.apache.curator:curator-recipes
 
-REM 全部包导出到lib
-REM call mvn dependency:copy-dependencies -DoutputDirectory=lib
+rem 全部包导出到lib
+rem call mvn dependency:copy-dependencies -DoutputDirectory=lib
 
-REM 只导出关联包到lib
-REM call mvn dependency:copy-dependencies -DoutputDirectory=lib -DincludeScope=compile
+rem 只导出关联包到lib
+rem call mvn dependency:copy-dependencies -DoutputDirectory=lib -DincludeScope=compile
 
-REM 出WAR包
-REM call mvn archetype:generate -DgroupId=nepxion -DartifactId=coroutine-common -Dversion=1.0.0-SNAPSHOT -DarchetypeArtifactId=maven-archetype-webapp -DarchetypeCatalog=internal
+rem 出WAR包
+rem call mvn archetype:generate -DgroupId=nepxion -DartifactId=coroutine-common -Dversion=1.0.0-SNAPSHOT -DarchetypeArtifactId=maven-archetype-webapp -DarchetypeCatalog=internal
 
-REM 出POM
-REM call mvn archetype:generate -DgroupId=nepxion -DartifactId=coroutine-common -DarchetypeArtifactId=maven-archetype-site-simple -DarchetypeCatalog=internal
+rem 出POM
+rem call mvn archetype:generate -DgroupId=nepxion -DartifactId=coroutine-common -DarchetypeArtifactId=maven-archetype-site-simple -DarchetypeCatalog=internal
 
-REM 出JAR包
-REM call mvn archetype:generate -DgroupId=nepxion -DartifactId=coroutine-common -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false -DarchetypeCatalog=internal 
+rem 出JAR包
+rem call mvn archetype:generate -DgroupId=nepxion -DartifactId=coroutine-common -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false -DarchetypeCatalog=internal 
 
-REM 加入到本地库
-REM call mvn install:install-file -DgroupId=kilim -DartifactId=kilim -Dversion=1.0 -Dfile=lib/kilim-1.0.jar -Dpackaging=jar -DgeneratePom=true
+rem 加入到本地库
+rem call mvn install:install-file -DgroupId=kilim -DartifactId=kilim -Dversion=1.0 -Dfile=lib/kilim-1.0.jar -Dpackaging=jar -DgeneratePom=true
 
 pause
