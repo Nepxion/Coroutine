927731c8ac879f884b01a576d1b46cf261b4b6de
Nepxion <1394997@qq.com>
2017-12-27 14:35:16 +0800

    增加Sh

---
+++ coroutine-framework-kilim/repo.sh
@@ -0,0 +1,18 @@
+echo 'on'
+echo '============================================================='
+echo '$                                                           $'
+echo '$                      Nepxion Coroutine                     $'
+echo '$                                                           $'
+echo '$                                                           $'
+echo '$                                                           $'
+echo '$  Nepxion Technologies All Right Reserved                  $'
+echo '$  Copyright(C) 2017                                        $'
+echo '$                                                           $'
+echo '============================================================='
+echo '.'
+echo 'off'
+
+title=Nepxion Coroutine
+color=0a
+
+mvn install:install-file -DgroupId=kilim -DartifactId=kilim -Dversion=1.0.1 -Dfile=repo/kilim/kilim/1.0.1/kilim-1.0.1.jar -Dpackaging=jar -DgeneratePom=true
