1124fb8542364a7252d7464946147ee61cbcfee4
Nepxion <1394997@qq.com>
2017-12-27 14:32:33 +0800

    增加Sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+echo 'on'
+echo '============================================================='
+echo '$                                                           $'
+echo '$                      Nepxion Coroutine                     $'
+echo '$                                                           $'
+echo '$                                                           $'
+echo '$                                                           $'
+echo '$  Nepxion Technologies All Right Reserved                  $'
+echo '$  Copyright(C) 2017                                        $'
+echo '$                                                           $'
+echo '============================================================='
+echo '.'
+echo 'off'
+
+title=Nepxion Coroutine
+color=0a
+
+mvn clean install -DskipTests
